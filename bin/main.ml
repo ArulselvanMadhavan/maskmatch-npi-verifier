9ed84129b33081ca2c883d8793727e0b37590aed
Arul Madhavan <arulselvan1234@gmail.com>
2020-05-04 04:56:07 -0700

    Basic regex setup

--- bin/main.ml
+++ bin/main.ml
@@ -5,7 +5,7 @@ open Npi_response
 open Yojson.Safe
 
 let body =
-  Client.get (Uri.of_string "https://npiregistry.cms.hhs.gov/api/?version=2.1&number=1111111111&pretty=true") >>= fun (resp, body) ->
+  Client.get (Uri.of_string "https://npiregistry.cms.hhs.gov/api/?version=2.1&number=1528427689&pretty=true") >>= fun (resp, body) ->
   let code = resp |> Response.status |> Code.code_of_status in
   Printf.printf "Response code: %d\n" code;
   Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
@@ -15,4 +15,8 @@ let body =
 
 let () =
   let body = Lwt_main.run body in
+  let npi_emacs_re = Re.Perl.re "\\b[0-9]{10}?" in
+  let npi_re = Re.Perl.compile npi_emacs_re in
+  let all_matches = Re.matches npi_re "some 0123456789 work1231231231" in
+  List.iter (print_endline) all_matches;
   print_endline ("Received body\n" ^ body)
--- lib/lib.ml
+++ lib/lib.ml
@@ -1,3 +1,1 @@
-open Npi_response.Npi_j
-
 let hello = "Hello world"
